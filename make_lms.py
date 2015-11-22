756f9fa7b09575f421697c040205af2b26ef8b59
Meital Singer <tali324@comcast.net>
2015-11-22 15:58:59 -0500

    lms and revised textnorm/transcripts

---
+++ make_lms.py
@@ -0,0 +1,24 @@
+import sys, os
+
+pizza_lm = sys.argv[1]
+swbd_lm = sys.argv[2]
+
+#make pizza lm
+os.system('~/srilm-1.7.1/bin/macosx/ngram-count -unk -map-unk -text {} -lm pizza_trigram.lm -wbdiscount'.format(pizza_lm))
+
+#make swbd lm
+os.system('~/srilm-1.7.1/bin/macosx/ngram-count -unk -map-unk -order 4 -text {} -lm swbd_4gram.lm -wbdiscount'.format(swbd_lm))
+
+#combine lm into one text file
+pizzafile = open(pizza_lm, 'r')
+swbdfile = open(swbd_lm, 'r')
+if 'swbd_pizza_lm' in os.listdir(os.getcwd()):
+	os.remove('swbd_pizza_lm')
+combined = open('swbd_pizza_lm', 'w')
+combined.write(pizzafile.read()+'\n'+swbdfile.read())
+pizzafile.close()
+swbdfile.close()
+combined.close()
+
+#create combined lm
+os.system('~/srilm-1.7.1/bin/macosx/ngram-count -unk -map-unk -order 4 -text swbd_pizza_lm -lm combined_4gram.lm -wbdiscount')
